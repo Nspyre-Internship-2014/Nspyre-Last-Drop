e5ed4de4b5214f876f2c747e13c4176768d4bc68
ionut-marcanasu <ionutmarcanasu@gmail.com>
2014-07-09 15:56:48 +0300

    placed c# arduino connection

---
+++ Server/test1connect/test1connect/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.Threading;
+
+namespace test1connect
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /*
+           SerialPort port = new SerialPort("COM3", 9600);
+            port.Open();
+            while (true)
+            {
+                  
+                string s1 = port.ReadLine();
+                Console.WriteLine(s1);
+              //  MessageBox.Show(s1);
+                Thread.Sleep(500);  
+                
+            }
+         */
+            ReadArduino ra1 = new ReadArduino();
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                string s2=ra1.getData();
+                Console.WriteLine(s2);
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+          
+                     
+        }
+    }
+}
---
+++ Server/test1connect/test1connect/ReadArduino.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO.Ports;
+
+namespace test1connect
+{
+    class ReadArduino
+    {
+        private static string s1;
+        public ReadArduino()
+        {
+            Thread t = new Thread(readData);      
+            t.Start();
+        }
+
+      private  static void readData()
+        {
+            SerialPort port = new SerialPort("COM3", 9600);
+            port.Open();
+            while (true)
+            {
+
+                s1 = port.ReadLine();
+              //  Console.WriteLine(s1);
+                //  MessageBox.Show(s1);
+                Thread.Sleep(1000);
+
+            }
+        }
+        
+      public string getData()
+      {
+          return s1;
+      }
+    }
+}
