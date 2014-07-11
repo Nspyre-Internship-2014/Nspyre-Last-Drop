da3c28871f2a548eae535ca60f42dc26b8f9687a
ionut-marcanasu <ionutmarcanasu@gmail.com>
2014-07-11 12:33:47 +0300

    Code about tutorial - client server test1

---
+++ CodeAboutClientServerTest/ClientApplication1/ClientApplication1/Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace ClientApplication1
+{
+    class Client
+    {
+
+        //The server ip address 
+        private static IPAddress ipAddress = IPAddress.Parse("10.33.90.52");
+
+        //The server port
+        private static Int32 myPort = 8021;
+
+        //The TCP Client object
+        private static TcpClient tcpclnt;
+
+        static void Main(string[] args)
+        {
+
+            //Start the connection to client
+            Thread t = new Thread(startConnection);
+            t.Start();
+
+        }
+
+        //Thread fucntion that connects to the server
+        static void startConnection()
+        {
+
+            //All code in a try catch
+            try
+            {
+                tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting...");
+
+                tcpclnt.Connect(ipAddress, myPort);
+                Console.WriteLine("Connected.");
+                Console.WriteLine("Enter the string to be sent:");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                tcpclnt.Close();
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ CodeAboutClientServerTest/ServerApplication1/ServerApplication1/Server.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerApplication1
+{
+    class Server
+    {
+
+        //The ip address 
+        private static IPAddress ipAddress = IPAddress.Parse("10.33.90.52");
+
+        //The port
+        private static Int32 myPort = 8021;
+
+        //The client listener
+        private static TcpListener myList;
+
+        static void Main(string[] args)
+        {
+
+            //Start thread that listens to the clients
+            Thread t = new Thread(startListening);
+            t.Start();
+
+        }
+
+        //Thread fucntion that listens to the clients
+        static void startListening()
+        {
+
+            //All code in a try catch
+            try
+            {
+                //Begin listening
+                myList = new TcpListener(ipAddress, myPort);
+                myList.Start(); //Start at that port
+
+                //Write some status info
+                Console.WriteLine("Server running on port " + myPort);
+                Console.WriteLine("Local end point: " + myList.LocalEndpoint);
+                Console.WriteLine("Waiting for clients...");
+
+
+                //Accept a connection from a client
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+                
+                //Close the socket
+                s.Close();
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                myList.Stop();
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
