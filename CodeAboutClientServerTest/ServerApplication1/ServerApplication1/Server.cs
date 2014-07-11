8f393f3d4e1372b35d219117cf4438b3cb7bd73b
ionut-marcanasu <ionutmarcanasu@gmail.com>
2014-07-11 12:43:26 +0300

    server updated with messages

--- CodeAboutClientServerTest/ServerApplication1/ServerApplication1/Server.cs
+++ CodeAboutClientServerTest/ServerApplication1/ServerApplication1/Server.cs
@@ -50,7 +50,18 @@ namespace ServerApplication1
                 //Accept a connection from a client
                 Socket s = myList.AcceptSocket();
                 Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
-                
+
+                 byte[] b = new byte[100];
+                 int k = s.Receive(b);
+                 Console.WriteLine("Recieved...");
+
+                 for (int i=0;i<k;i++)
+                 {
+                 Console.Write(Convert.ToChar(b[i]));
+                    }
+                 ASCIIEncoding asen = new ASCIIEncoding();
+                 s.Send(asen.GetBytes("Automatic message: " + "String received by server!"));
+                Console.WriteLine("\n Automatic message sent!");
                 //Close the socket
                 s.Close();
 
