5a4a9fa869c182a8f15c29eebd92be18084a85f9
ionut-marcanasu <ionutmarcanasu@gmail.com>
2014-07-14 15:24:14 +0300

    single server multiple clients architecture with string data

---
+++ ServerMultipleClients1/ClientForm1/ClientForm1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ClientForm1
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public void msg(string mesg)
+        {
+            textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + mesg;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes("Message from Client$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10025];
+            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            msg("Data from Server : " + returndata);
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            msg("Client Started");
+            int myPort = 8021;
+            clientSocket.Connect("10.33.90.52", myPort);
+            label1.Text = "Client Socket Program - Server Connected ...";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ServerMultipleClients1/ServerApplication1/ServerApplication1/Server.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * This is the working code  for the tutorial found here:
+ * http://codeabout.wordpress.com/2011/03/06/building-a-simple-server-client-application-using-c/
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerApplication1
+{
+    class Server
+    {
+
+        //The ip address 
+        private static IPAddress ipAddress = IPAddress.Parse("10.33.90.52");
+
+        //The port
+        private static Int32 myPort = 8021;
+
+
+       private static TcpClient clientSocket=default(TcpClient);
+
+       private static int counter = 0;
+
+       private static TcpListener socketServer = new TcpListener(ipAddress, myPort);
+
+        static void Main(string[] args)
+        {
+
+            //Start thread that listens to the clients
+            Thread t = new Thread(startListening);
+            t.Start();
+
+        }
+
+        //Thread fucntion that listens to the clients
+        static void startListening()
+        {
+
+            //All code in a try catch
+            try
+            {
+                //Begin listening
+              
+                socketServer.Start(); //Start at that port
+
+                //Write some status info
+                Console.WriteLine("Server running on port " + myPort);
+                Console.WriteLine("Local end point: " + socketServer.LocalEndpoint);
+                Console.WriteLine("Waiting for clients...");
+
+                while (true)
+                {
+                    counter=counter+1;
+                    //Accept a connection from a client
+                    clientSocket = socketServer.AcceptTcpClient();
+                    string clientIPAddress = ((IPEndPoint)clientSocket.Client.RemoteEndPoint).Address.ToString();
+                    Console.WriteLine("Connection accepted from " + clientIPAddress);
+
+                      handleClinet client = new handleClinet();
+                 client.startClient(clientSocket, Convert.ToString(counter));
+            
+                    /*
+                    byte[] b = new byte[100];
+                    int k = clientSocket.Receive(b);
+                    Console.WriteLine("Recieved...");
+
+                    for (int i = 0; i < k; i++)
+                    {
+                        Console.Write(Convert.ToChar(b[i]));
+                    }
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    clientSocket.Send(asen.GetBytes("Automatic message: " + "String received by server!"));
+                    Console.WriteLine("\n Automatic message sent!");
+                    */
+                }
+                    //Close the socket
+              
+                clientSocket.Close();
+                socketServer.Stop();
+                Console.WriteLine(" >> " + "exit");
+                Console.ReadLine();
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                socketServer.Stop();
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
