c8f5c9ddf41707e7d4c0ed91209a80572d8a8983
ionut-marcanasu <ionutmarcanasu@gmail.com>
2014-07-24 11:59:08 +0300

    client test communication

---
+++ Archive/TrayApplicationTest_IonutM/TrayApplicationTest/forms/LoginForm.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace TrayApplicationTest
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.notifyIcon2 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notificationOptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(113, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sign in";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(259, 148);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(207, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(143, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Keep me signed in";
+            // 
+            // notifyIcon2
+            // 
+            this.notifyIcon2.ContextMenuStrip = this.contextMenuStrip2;
+            this.notifyIcon2.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon2.Icon")));
+            this.notifyIcon2.Text = "notifyIcon2";
+            this.notifyIcon2.Visible = true;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem,
+            this.notificationOptionsToolStripMenuItem,
+            this.logOutToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(183, 92);
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.testToolStripMenuItem.Text = "Take care of a plant";
+            this.testToolStripMenuItem.Click += new System.EventHandler(this.takeCarePlantMenuStrip_click);
+            // 
+            // notificationOptionsToolStripMenuItem
+            // 
+            this.notificationOptionsToolStripMenuItem.Name = "notificationOptionsToolStripMenuItem";
+            this.notificationOptionsToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.notificationOptionsToolStripMenuItem.Text = "Notification Options";
+            this.notificationOptionsToolStripMenuItem.Click += new System.EventHandler(this.notificationOptionsToolStripMenuItem_Click);
+            // 
+            // logOutToolStripMenuItem
+            // 
+            this.logOutToolStripMenuItem.Name = "logOutToolStripMenuItem";
+            this.logOutToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.logOutToolStripMenuItem.Text = "LogOut";
+            this.logOutToolStripMenuItem.Click += new System.EventHandler(this.logOutToolStripMenuItem_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 257);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NotifyIcon notifyIcon2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem notificationOptionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logOutToolStripMenuItem;
+    }
+}
---
+++ Archive/TrayApplicationTest_IonutM/TrayApplicationTest/forms/TakeCareOfAPlant.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace TrayApplicationTest
+{
+      [CallbackBehavior(UseSynchronizationContext = false)]
+    public partial class TakeCareOfAPlant : Form , IMyServiceCallback
+    {
+
+        DuplexChannelFactory<IServicesWCF> netTcpFactory;
+        IServicesWCF serviceProvider;
+        private string mail;
+        private string password;
+        SynchronizationContext _SyncContext = null;
+
+        public void ServerConnect()
+        {
+            NetTcpBinding netTcp = new NetTcpBinding();
+         //   MyServiceCallback serviceCallback = new MyServiceCallback();
+
+            netTcp.Security.Mode = SecurityMode.None;
+            netTcpFactory = new DuplexChannelFactory<IServicesWCF>(this, netTcp, new EndpointAddress(
+                  "net.tcp://10.33.92.30:8021/Service1/LastDropService"));
+
+            _SyncContext = SynchronizationContext.Current;
+            serviceProvider = netTcpFactory.CreateChannel();
+            serviceProvider.subscribe();
+        }
+
+        public void OnCallback()
+        {
+            SendOrPostCallback refreshList = delegate
+            {
+                getAvailablePlants();
+                getSubscribedPlants();
+            };
+            _SyncContext.Post(refreshList, null);
+        }
+
+        public TakeCareOfAPlant(string mail, string password)
+        {
+            this.mail = mail;
+            this.password = password;
+            ServerConnect();
+            InitializeComponent();
+        }
+
+        private void TakeCareOfAPlant_Load(object sender, EventArgs e)
+        {
+            getAvailablePlants();
+            getSubscribedPlants();
+        }
+
+        private void TakeCareOfAPlant_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serviceProvider.unsubscribe();
+            netTcpFactory.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i)==true)
+                {
+                    string plantName = checkedListBox1.Items[i].ToString();
+                     
+                    serviceProvider.unsubscribeToPlant(mail, plantName);
+                }          
+            }
+        //    getSubscribedPlants();
+        //    getAvailablePlants();
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+             
+            for (int i = 0; i < checkedListBox2.Items.Count; i++)
+            {
+                if (checkedListBox2.GetItemChecked(i) == true)
+                {
+                    string plantName = checkedListBox2.Items[i].ToString();
+                    
+                    serviceProvider.subscribeToPlant(mail, plantName);
+                }
+            }
+        //    getAvailablePlants();
+        //    getSubscribedPlants();
+        }
+
+        public void getSubscribedPlants() 
+        {
+            checkedListBox1.Items.Clear();
+            string subscribed = serviceProvider.getSubscribedPlants(mail, password);
+            Console.WriteLine(subscribed);
+            var serializer2 = new XmlSerializer(typeof(List<Plant>));
+            List<Plant> subscribedPlantList;
+            using (TextReader reader = new StringReader(subscribed))
+            {
+                subscribedPlantList = (List<Plant>)serializer2.Deserialize(reader);
+            }
+            foreach (Plant plant in subscribedPlantList)
+            {
+
+                checkedListBox1.Items.Add(plant.Name);
+            }
+        }
+
+        public void getAvailablePlants() 
+        {
+            checkedListBox2.Items.Clear();
+            string list = serviceProvider.getAvailablePlants(mail, password);
+
+            var serializer = new XmlSerializer(typeof(List<Plant>));
+            List<Plant> plantList;
+            using (TextReader reader = new StringReader(list))
+            {
+                plantList = (List<Plant>)serializer.Deserialize(reader);
+            }
+            foreach (Plant plant in plantList)
+            {
+                checkedListBox2.Items.Add(plant.Name);
+            }
+        }
+
+
+
+
+    }
+}
